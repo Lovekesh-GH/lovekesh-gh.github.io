be899fffd1cbfe45926e6f24440cc5de4fbae2da
LAKKI <thakurluckysinghbrh@gmail.com>
2021-05-30 00:15:30 +0530

    partially done

--- schedule.js
+++ schedule.js
@@ -11,6 +11,8 @@ if(!localStorage.getItem('weekData')){
         [0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0],
+
+        [0,0,0,0,0,0,0],
     ]
 localStorage.setItem('weekData',JSON.stringify(emptyMat))
 }
@@ -25,11 +27,27 @@ function askUser(row,column){
 let usrSaidYes=confirm('set this task as done?');
 if(usrSaidYes){
     stateSet(row,column,1);
+    document.querySelectorAll('td')[(row+1)*8+column+1].style.color='green'
 
 }else{
-    stateSet(row,column,-1)
+    stateSet(row,column,-1);
+    document.querySelectorAll('td')[(row+1)*8+column+1].style.color='red'
 }
 }
 function stateGet(row,column) {
     return JSON.parse(localStorage.getItem('weekData'))[row][column]
 }
+function colorise(){
+    let weekData=JSON.parse( localStorage.getItem('weekData'));
+    for(let row=0;row<11;row++){
+        for(let col=0;col<7;col++){
+            let state=weekData[row][col];
+          if(state==1)  document.querySelectorAll('td')[(row+1)*8+col+1].style.color='green'
+        else  if(state==-1)  document.querySelectorAll('td')[(row+1)*8+col+1].style.color='red'
+        //  else if(state==0)  document.querySelectorAll('td')[(row+1)*8+col+1].style.color='yellow'
+        // else debugger
+        }
+    }
+
+}
+colorise()
